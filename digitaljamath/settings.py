7d6d858e6a961f48316637f2013f5cf78f4e5982
Mohammed Azzan Patni <azzaxp@gmail.com>
2025-12-29 01:02:21 +0530

    fix: add STATIC_ROOT, rename containers to digitaljamath

--- digitaljamath/settings.py
+++ digitaljamath/settings.py
@@ -116,11 +116,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 # DRF & JWT Configuration
 REST_FRAMEWORK = {
